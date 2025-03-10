8c7d48142a6acf1182c0c591bf4a133adecce7b9
Bao Tran <bao.tran080898@gmail.com>
2025-03-10 16:40:23 +0700

    banner slide bar at home page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,8 +1,46 @@
 import { Card } from 'antd';
+import Slider from 'react-slick';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import './Home.css';
 
 const Home = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+  };
+
+  const banners = [
+    '/banners/banner_1.jpg',
+    '/banners/banner_2.jpg',
+    '/banners/banner_3.jpg',
+    // Thêm các banner khác tại đây
+  ];
+
   return (
     <Card title="Trang chủ">
+      <div className="banner-slider">
+        <Slider {...settings}>
+          {banners.map((banner, index) => (
+            <div key={index}>
+              <img 
+                src={banner} 
+                alt={`Banner ${index + 1}`}
+                style={{
+                  width: '100%',
+                  height: '400px',
+                  objectFit: 'cover'
+                }}
+              />
+            </div>
+          ))}
+        </Slider>
+      </div>
       <p>Chào mừng đến với hệ thống</p>
     </Card>
   );
